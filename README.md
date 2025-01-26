bbc0e74288bc81f2de2841bc566710b4b3c9b5fe
TanayBPatel <tanay6451@gmail.com>
2025-01-26 15:16:29 +0530

    final server

--- README.md
+++ README.md
@@ -17,3 +17,4 @@
 # frontendkumbh
 # frontendkumbh
 # frontendkumbh
+# frontendkumbh
--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const port = 4000;
 app.use(express.json());
 app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: 'https://one-1r62.onrender.com', // Replace with your frontend's deployed URL
       credentials: true,
     })
   );
