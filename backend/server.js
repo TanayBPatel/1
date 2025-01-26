5325873e6879db7dd78aa5afe814cbc1e123447b
TanayBPatel <tanay6451@gmail.com>
2025-01-26 14:23:09 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import path from 'path'
 const app= express();
 app.use(express.json())
 const __dirname = path.resolve();
-require('dotenv').config();
+const dotenv = require('dotenv').config();
 const port =  4000;
 const dbconnection = require('./model/dbconnection')
 const Schema = require("./schema/dbschema.js");
